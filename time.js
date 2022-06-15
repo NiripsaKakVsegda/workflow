d782adfb404b88cc9b1f4b7918c0a12ae15db6c1
alyona <anna.poluyanenko@gmail.com>
2022-06-15 14:46:37 +0500

    time done

--- time.js
+++ time.js
@@ -1,8 +1,9 @@
 let datetime = new Date().toLocaleString();
-document.getElementById("time").textContent = datetime;
+document.getElementById("time").textContent = datetime.substring(0, datetime.length - 3);
 
 function refreshTime() {
     const timeDisplay = document.getElementById("time");
-    timeDisplay.textContent = new Date().toLocaleString();
+    let a = new Date().toLocaleString();
+    timeDisplay.textContent = a.substring(0, a.length - 3);
 }
 setInterval(refreshTime, 1000);
