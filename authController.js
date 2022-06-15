cd99a731ef025e85ec58f7418f7caa6d36e49862
amagoose <amagooseoj@mail.ru>
2022-06-15 19:44:37 +0500

    some backend

--- authController.js
+++ authController.js
@@ -1,51 +1,44 @@
+const User = require('./models/User')
+const Role = require('./models/Role')
+
 class authController {
     async registration(req, res) {
         try {
-            const errors = validationResult(req)
-            if (!errors.isEmpty()) {
-                return res.status(400).json({message: "Ошибка при регистрации", errors})
-            }
-            const {username, password} = req.body;
+            const {username, userEmail, password} = req.body
             const candidate = await User.findOne({username})
             if (candidate) {
-                return res.status(400).json({message: "Пользователь с таким именем уже существует"})
+                return res.statusCode(400).json({message: 'Пользователь с таким именем уже существует'})
             }
-            const hashPassword = bcrypt.hashSync(password, 7);
-            const userRole = await Role.findOne({value: "USER"})
-            const user = new User({username, password: hashPassword, roles: [userRole.value]})
-            await user.save()
-            return res.json({message: "Пользователь успешно зарегистрирован"})
+            const emailCandidate = await User.findOne({email: userEmail})
+            if (emailCandidate) {
+                return res.statusCode(400).json({message: 'Пользователь с такой электронной почтой уже существует'})
+            }
+            const
         } catch (e) {
             console.log(e)
-            res.status(400).json({message: 'Registration error'})
+            res.status(400).json({message: 'ошибка регистрации'})
         }
     }
 
     async login(req, res) {
         try {
-            const {username, password} = req.body
-            const user = await User.findOne({username})
-            if (!user) {
-                return res.status(400).json({message: `Пользователь ${username} не найден`})
-            }
-            const validPassword = bcrypt.compareSync(password, user.password)
-            if (!validPassword) {
-                return res.status(400).json({message: `Введен неверный пароль`})
-            }
-            const token = generateAccessToken(user._id, user.roles)
-            return res.json({token})
+
         } catch (e) {
             console.log(e)
-            res.status(400).json({message: 'Login error'})
+            res.status(400).json({message: 'ошибка входа в систему'})
         }
     }
 
     async getUsers(req, res) {
         try {
-            const users = await User.find()
-            res.json(users)
+            const candidate = await User.findOne({email:'amagooseoj@mail.ru'})
+            if (candidate) {
+                res.json('i found u')
+            } else {
+                res.json('okay boomer')
+            }
         } catch (e) {
-            console.log(e)
+
         }
     }
 }
