0f0d3b90515849db7a2c55e828d18df087f077aa
invis166 <kolesnikovv028@gmail.com>
2022-06-18 00:14:45 +0500

    weekday code refactoring

--- public/js/time.js
+++
@@ -1,9 +0,0 @@
-let datetime = new Date().toLocaleString();
-document.getElementById("time").textContent = datetime.substring(0, datetime.length - 3);
-
-function refreshTime() {
-    const timeDisplay = document.getElementById("time");
-    let a = new Date().toLocaleString();
-    timeDisplay.textContent = a.substring(0, a.length - 3);
-}
-setInterval(refreshTime, 1000);
