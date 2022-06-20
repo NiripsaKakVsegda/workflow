178c76e6cd120c364075c794ceab0b25f7247cf5
Hvv <hvv7007@gmail.com>
2022-06-20 08:49:05 +0500

    deadline fix

--- service/find_nearest_deadline.js
+++ service/find_nearest_deadline.js
@@ -23,13 +23,15 @@ async function findNearestDeadlineForUser(user) {
     let first = new Date(curr.setDate(curr.getDate() - (weekday))).toISOString().split('T')[0];
     let last = new Date(curr.setDate(curr.getDate() + 7)).toISOString().split('T')[0];
 
-    taskDoneArray = taskDoneArray.filter((el) => first <= el['endTime'].toISOString().split('T')[0]
+    let currWeekTaskDoneArray = taskDoneArray.filter((el) => first <= el['endTime'].toISOString().split('T')[0]
         & el['endTime'].toISOString().split('T')[0] <= last);
-    taskArray = taskArray.filter((el) => first <= el['endTime'].toISOString().split('T')[0]
+    let currWeekTaskArray = taskArray.filter((el) => first <= el['endTime'].toISOString().split('T')[0]
         & el['endTime'].toISOString().split('T')[0] <= last);
 
-    let donePercent = (taskDoneArray.length / taskArray.length || 0)  * 100;
+    let donePercent = (currWeekTaskDoneArray.length / currWeekTaskArray.length || 0)  * 100;
+    console.log(taskArray)
     taskArray = taskArray.filter((el) => el['endTime'].getTime() >= new Date().getTime());
+    console.log(taskArray)
     taskArray = taskArray.filter((el) => taskDoneArray.filter((e) => e._id.equals(el._id)).length === 0);
 
     taskArray.sort((a, b) => a['endTime'].getTime() >= b['endTime'].getTime() ? 1 : -1);
