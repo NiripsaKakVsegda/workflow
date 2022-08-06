798223e0d484ac2566b88ef0366e81c9bd953d88
user <user@user.user>
2022-08-06 15:00:16 +0300

    add urgent feature

--- service/update_task_progress.js
+++ service/update_task_progress.js
@@ -3,11 +3,15 @@ const Task = require("../models/Task");
 async function updateTaskProgress (req, res) {
     const taskId = req.params.taskId.slice(1);
     const isDone = req.body.progress === 'true';
+    const isUrgent = req.body.urgent === 'true';
+
     const user = await getUser(req);
     let task;
 
     try {
         task = await Task.findById(taskId);
+        task.isUrgent = isUrgent;
+        await task.save();
     } catch (e) {
         return res
             .status(400)
@@ -17,7 +21,7 @@ async function updateTaskProgress (req, res) {
     if (isDone) {
         user.tasksDone.push(task._id);
     } else {
-        user.tasksDone = user.tasksDone.filter(function(e) { return e.valueOf() != taskId });
+        user.tasksDone = user.tasksDone.filter(function(e) { return e.valueOf() !== taskId });
     }
     user.save();
     res.redirect('/schedule');
